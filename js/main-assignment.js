1ceba17b18378ee0f99da62ccfc0b39de8623a98
Noppakhun230 <77983064+Noppakhun230@users.noreply.github.com>
2021-12-16 06:50:03 +0000

    Deploying to gh-pages from @ camt-mmit/2021_02-954447-001-lab02-javascript-and-dom-Noppakhun230@450a7940dedcf2af78a9e91fa5af2c4cc45108e7 🚀

---
+++ js/main-assignment.js
@@ -0,0 +1,8 @@
+import { createSectionInput } from './section-input.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mainContainer = document.querySelector('.cmp-main-container');
+
+    createSectionInput(mainContainer);
+});
+    
